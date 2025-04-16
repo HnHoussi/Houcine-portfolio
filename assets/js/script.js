c1b82010e73767a8a38851a7f73ac6bba515db90
Houcine <houcinehannoun@gmail.com>
2025-04-16 17:39:24 +0200

    Add hamburger icon styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@ $(window).on("scroll", function () {
   let scroll = $(window).scrollTop();
   if (scroll >= 50) {
     $(".sticky").addClass("stickyadd");
-    $(".hamburger-color").removeClass("text-white");
+    $(".hamburger-icon").removeClass("text-white");
   } else {
     $(".sticky").removeClass("stickyadd");
-    $(".hamburger-color").addClass("text-white");
+    $(".hamburger-icon").addClass("text-white");
   }
+});
+
+$(".navbar-toggler").on("click", function () {
+  $(".hamburger-icon").toggleClass("fa-bars fa-times");
 });
