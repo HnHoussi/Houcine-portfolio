3e2a606e1e52752571ba8c02939df3880f7f6a29
Houcine <houcinehannoun@gmail.com>
2025-04-17 18:03:36 +0200

    Adding the home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,15 @@ $(window).on("scroll", function () {
 $(".navbar-toggler").on("click", function () {
   $(".hamburger-icon").toggleClass("fa-bars fa-times");
 });
+
+// Title animation
+
+const typed = new Typed("#typed", {
+  strings: ["Houcine HANNOUN", "un Developpeur Web", "un Ingénieur Industriel"],
+  typeSpeed: 70,
+  backSpeed: 35,
+  backDelay: 1000,
+  startDelay: 300,
+  loop: true,
+  showCursor: false
+});
